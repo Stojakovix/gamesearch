1507f35024380a645ce600b7593990fb2d7eb2ee
boro1862 <bruno.stojak@gmail.com>
2024-01-05 15:06:02 +0100

    test

--- src/Conn.js
+++ src/Conn.js
@@ -5,11 +5,7 @@ const axiosConn = require("axios");
 const fetchGames = async (searchTerm ='', pageSize = 20) => {
   const options = {
     method: "GET",
-    url: 'https://rawg-video-games-database.p.rapidapi.com/games/%7Bgame_pk%7D',
-    headers: {
-      "X-RapidAPI-Key": "979c11dea69548709ad941d3a96b2c85",
-      "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
-    },
+    url: 'https://www.freetogame.com/api/games',
     params: {
       search: searchTerm,
       page_size:pageSize,
